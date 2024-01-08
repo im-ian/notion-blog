fc976aaf4f7af9bfda6fc16b055575b0a26ba0ba
ian <38205068+im-ian@users.noreply.github.com>
2024-01-08 18:32:44 +0900

    Fix: add Inconsolata google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,20 @@ export default async function RootLayout({
 
   return (
     <html lang={"en"}>
+      <head>
+        <link rel={"preconnect"} href={"https://fonts.googleapis.com"} />
+        <link
+          rel={"preconnect"}
+          href={"https://fonts.gstatic.com"}
+          crossOrigin={"anonymous"}
+        />
+        <link
+          href={
+            "https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500&display=swap"
+          }
+          rel={"stylesheet"}
+        />
+      </head>
       <body>
         <NotionPageProvider pages={pages}>
           <>
