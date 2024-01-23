5d383a3ee256785f05430c8ee873d2ec87d44f58
ian <38205068+im-ian@users.noreply.github.com>
2024-01-23 16:00:34 +0900

    Fix: add PostCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import { Card, Flex, Layout } from "@/components/Layouts";
-import { Tags } from "@/components/Tags";
-import { Date as DateText, Heading } from "@/components/Texts";
-import { Routes } from "@/constants";
+import { Layout } from "@/components/Layouts";
+import { PostCard } from "@/components/Posts";
 import { sprinkles } from "@/css/sprinkles.css";
 import { getPosts } from "@/services/notion";
 
@@ -14,39 +12,8 @@ async function Home() {
         padding: "medium",
       })}
     >
-      {pages.map(({ value }) => {
-        const { attributes } = value;
-
-        return (
-          <a
-            key={attributes.slug.value}
-            href={Routes.Post(attributes.slug.value || "")}
-          >
-            <Card>
-              <Heading size={"2x"} tint>
-                {attributes.title.value}
-              </Heading>
-              <p>{attributes.summary.value}</p>
-              <Flex>
-                {attributes.category.value && (
-                  <Tags
-                    options={attributes.category.options}
-                    tags={attributes.category.value}
-                  />
-                )}
-                {attributes.tags.value && (
-                  <Tags
-                    options={attributes.tags.options}
-                    tags={attributes.tags.value}
-                  />
-                )}
-              </Flex>
-              {attributes.date.value && (
-                <DateText date={attributes.date.value} />
-              )}
-            </Card>
-          </a>
-        );
+      {pages.map((page) => {
+        return <PostCard key={page.value.attributes.slug.value} page={page} />;
       })}
     </Layout>
   );
