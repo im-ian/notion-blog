cab00e7b96addf4e94f570a4234f3ae924a03fdd
ian <38205068+im-ian@users.noreply.github.com>
2024-01-30 14:41:19 +0900

    Fix: meta template 변경

--- site.config.ts
+++ site.config.ts
@@ -9,7 +9,7 @@ const CONFIG: Config = {
   meta: {
     title: {
       default: "다람쥐 헌 쳇바퀴에 타고파",
-      template: "다람쥐 %s에 타고파",
+      template: "%s",
     },
     description: "My Blog Description",
   },
