c34e7d8d02c2189e01fd58b483b68e6b1d8dbfc4
ian <38205068+im-ian@users.noreply.github.com>
2024-02-13 18:18:41 +0900

    Feature: 로딩 인디케이터 추가

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import BounceLoader from "react-spinners/BounceLoader";
 
+import { Box } from "../Layouts";
+
+import { vars } from "@/css/vars.css";
 import useDarkMode from "@/hooks/useDarkMode";
 import { getSiteConfig } from "@/utils/config";
 
@@ -9,9 +13,12 @@ function Comment() {
   const commentRef = useRef<HTMLDivElement>(null);
 
   const isDarkMode = useDarkMode();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (isDarkMode === undefined) return;
+
+    setIsLoading(true);
 
     const [$utterances] = document.getElementsByClassName("utterances");
     const [$comment] = document.getElementsByClassName("utterances-frame");
@@ -31,11 +38,21 @@ function Comment() {
     script.setAttribute("label", "comment");
     script.setAttribute("theme", theme);
     script.setAttribute("crossorigin", "anonymous");
+    script.onload = () => setIsLoading(false);
 
     commentRef.current?.appendChild(script);
   }, [isDarkMode]);
 
-  return <div ref={commentRef}></div>;
+  return (
+    <div ref={commentRef}>
+      {isLoading && (
+        <BounceLoader
+          cssOverride={{ margin: vars.space["center"] }}
+          color={vars.color["brand-400"]}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Comment;
