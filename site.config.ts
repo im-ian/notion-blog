0ba84a0080e0b2d20f9aa12d432cbb07083dbc44
ian <38205068+im-ian@users.noreply.github.com>
2024-03-26 19:08:02 +0900

    Fix: remove url config

--- site.config.ts
+++ site.config.ts
@@ -23,10 +23,6 @@ const CONFIG: Config = {
   },
   site: {
     title: "다람쥐 헌 쳇바퀴에 타고파",
-    domain: {
-      dev: process.env.DEV_URL || "",
-      prod: process.env.PROD_URL || "",
-    },
     // 포스트 캐시 갱신 시간
     postRevalidate: 60 * 60, // 1 hour
     // 검색 단축키 사용 여부
